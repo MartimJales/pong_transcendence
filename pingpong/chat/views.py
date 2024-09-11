4d7c6294e18159e9c4f905de61a85c0251c90265
Le Santos <leandrotyr@gmail.com>
2024-09-11 10:47:50 +0100

    chat app funcional

--- pingpong/chat/views.py
+++ pingpong/chat/views.py
@@ -4,13 +4,12 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
-from . forms import UserCreateForm
+from authentication.forms import UserCreateForm
 from django.views.decorators.csrf import csrf_exempt
 
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.decorators import login_required
-from .models import Match
 from django.contrib.auth.models import User
 import json
 from datetime import datetime
--- pingpong/pingpong/settings.py
+++ pingpong/pingpong/settings.py
@@ -43,17 +43,19 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     #'authentication',
     'authentication.apps.AuthenticationConfig',
+    'chat',
+    'corsheaders', 
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'pingpong.urls'
