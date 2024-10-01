a2851ab10c501642eee2bbed611a8ed082152cbf
Pac4games <paulo.nicolau7@gmail.com>
2024-10-01 19:35:57 +0100

    ModSecurity implemented sucessfully

--- nginx/ModSecurity/setup_modsecurity.sh
+++ nginx/ModSecurity/setup_modsecurity.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Moving CRS files to correct location and applying custom configuration
-mv /opt/owasp-crs/ /usr/local/
+# Linking CRS files to a different location and applying custom configuration
+ln -s /opt/owasp-crs /usr/local/owasp-crs
 # mv crs-setup.conf /usr/local/owasp-crs/
 
 # Renaming example files to be functional
@@ -11,4 +11,6 @@ done
 
 # Applying ModSecurity configuration
 mkdir /etc/nginx/modsecurity
-# mv modsecurity.conf /etc/nginx/modsecurity/
+ln -s /etc/modsecurity.d/unicode.mapping /etc/nginx/modsecurity/unicode.mapping
+mv /modsecurity.tmp.d/main.conf /etc/nginx/modsecurity/
+mv /modsecurity.tmp.d/modsecurity.conf /etc/nginx/modsecurity/
