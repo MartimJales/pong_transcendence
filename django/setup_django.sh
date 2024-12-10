50a49b6e50f507a532c0a10c5f1646b95140cac3
Pac4games <paulo.nicolau7@gmail.com>
2024-12-10 20:18:07 +0000

    Fixed some issues, ready to port to most recent branch

--- django/setup_django.sh
+++ django/setup_django.sh
@@ -6,19 +6,6 @@ while [ ! -f /setup/vault_token ]; do
     sleep 2
 done
 
-# Fetch ROLE_ID
-#VAULT_ROLE_ID=$(curl -s --header "X-Vault-Token: $VAULT_TOKEN" \
-#    --request GET \
-#    $VAULT_ADDR/v1/auth/approle/role/django-role/role-id | jq -r '.data.role_id')
-
-# Fetch SECRET_ID
-#VAULT_SECRET_ID=$(curl -s --header "X-Vault-Token: $VAULT_TOKEN" \
-#    --request POST \
-#    $VAULT_ADDR/v1/auth/approle/role/django-role/secret-id | jq -r '.data.secret_id')
-
-#export VAULT_ROLE_ID
-#export VAULT_SECRET_ID
-
 export VAULT_KEYS=$(cat /setup/vault_keys)
 export VAULT_TOKEN=$(cat /setup/vault_token)
 
